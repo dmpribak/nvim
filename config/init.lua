a56d56710017cb36533735bcf95686b1863f7b9d
Dillan Pribak <dmpribak@umich.edu>
2025-09-13 17:01:19 +0000

    new config

--- config/init.lua
+++ config/init.lua
@@ -8,7 +8,10 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.showmatch = true
 vim.opt.matchtime = 3
 vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
 vim.cmd("syntax on")
+vim.g.mapleader = " "
 
 -- Keybinds --
 vim.keymap.set({"i", "t"}, "jk", "<C-\\><C-n>")
@@ -23,6 +26,32 @@ vim.keymap.set({"i", "n", "t"}, "<A-l>", "<C-\\><C-n><C-w>l")
 
 vim.keymap.set("n", "<C-k>", ":Neotree toggle reveal<cr>")
 
+-- venn.nvim: enable or disable keymappings
+function _G.Toggle_venn()
+    local venn_enabled = vim.inspect(vim.b.venn_enabled)
+    if venn_enabled == "nil" then
+        vim.b.venn_enabled = true
+        vim.cmd[[setlocal ve=all]]
+        -- draw a line on HJKL keystokes
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+        -- draw a box by pressing "f" with visual selection
+        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
+    else
+        vim.cmd[[setlocal ve=]]
+        vim.api.nvim_buf_del_keymap(0, "n", "J")
+        vim.api.nvim_buf_del_keymap(0, "n", "K")
+        vim.api.nvim_buf_del_keymap(0, "n", "L")
+        vim.api.nvim_buf_del_keymap(0, "n", "H")
+        vim.api.nvim_buf_del_keymap(0, "v", "f")
+        vim.b.venn_enabled = nil
+    end
+end
+-- toggle keymappings for venn using <leader>v
+vim.api.nvim_set_keymap('n', '<leader>v', ":lua Toggle_venn()<CR>", { noremap = true})
+
 -- Plugins --
 require("config.lazy")
 
@@ -36,6 +65,7 @@ require("nvim-treesitter.configs").setup {
 
 vim.lsp.enable("pyright")
 vim.lsp.enable("nixd")
+vim.lsp.enable("lua_ls")
 
 local cmp = require("cmp")
 
@@ -82,6 +112,39 @@ require("lspconfig").nixd.setup({
         }
     }
 })
+require("lspconfig")["lua_ls"].setup {
+    on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc')) then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
 
 require("toggleterm").setup{
     open_mapping = [[<C-\>]]
@@ -122,3 +185,21 @@ vim.api.nvim_create_user_command("Format", function(args)
   end
   require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
+
+require("neo-tree").setup({
+    window = {
+        mappings = {
+            ["I"] = {
+                function(state)
+                    local node = state.tree:get_node()
+                    -- print("yanked", node.path)
+                    vim.fn.setreg("", node.path)
+                    vim.fn.setreg("0", node.path)
+                    vim.fn.setreg("*", node.path)
+                    vim.fn.setreg("+", node.path)
+                end,
+                desc = "copy path"
+            }
+        }
+    }
+})
